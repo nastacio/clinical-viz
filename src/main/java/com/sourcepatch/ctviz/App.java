efd7689c234cab2a486588e8d8e795856d3a6211
Denilson Nastacio <dnastacio@gmail.com>
2017-02-20 10:16:28 -0500

    Adding various fields to clinical trials (age, gender, statuses)

--- src/main/java/com/sourcepatch/ctviz/App.java
+++ src/main/java/com/sourcepatch/ctviz/App.java
@@ -16,8 +16,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Map;
-import java.util.TreeMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
@@ -26,16 +29,19 @@ import java.util.zip.ZipInputStream;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.io.graphml.GraphMLWriter;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
+import com.sourcepatch.ctviz.ctgov.AddressStruct;
+import com.sourcepatch.ctviz.ctgov.AgencyClassEnum;
 import com.sourcepatch.ctviz.ctgov.ClinicalStudy;
-import com.sourcepatch.ctviz.ctgov.EnrollmentStruct;
+import com.sourcepatch.ctviz.ctgov.EligibilityStruct;
+import com.sourcepatch.ctviz.ctgov.FacilityStruct;
 import com.sourcepatch.ctviz.ctgov.SponsorsStruct;
-import com.sourcepatch.ctviz.ctgov.StudyTypeEnum;
 
 /**
  * Converts a clinical trial search into a graph visualization.
@@ -43,6 +49,15 @@ import com.sourcepatch.ctviz.ctgov.StudyTypeEnum;
  * @author Denilson Nastacio
  */
 public class App {
+
+	private static final String NCT_DATE_PATTERN_1 = "MMMMM yyyy";
+	private static final String NCT_DATE_PATTERN_2 = "MMMMM dd, yyyy";
+	private static final SimpleDateFormat NCT_DATE_FORMAT_1 = new SimpleDateFormat(NCT_DATE_PATTERN_1);
+	private static final SimpleDateFormat NCT_DATE_FORMAT_2 = new SimpleDateFormat(NCT_DATE_PATTERN_2);
+
+	private static final String VERTEX_PROPERTY_LOCATION_FULL_ADDRESS = "location_full_address";
+	private static final String VERTEX_PROPERTY_CONDITION_NAME = "condition_name";
+	private static final String VERTEX_PROPERTY_SPONSOR_NAME = "sponsor_name";
 
 	/*
 	 * Ensuring default logging properties are loaded
@@ -101,9 +116,6 @@ public class App {
 		JAXBContext jc = JAXBContext.newInstance(instancePath);
 		Unmarshaller u = jc.createUnmarshaller();
 
-		Map<String, Vertex> sponsorMap = new TreeMap<>();
-		Map<String, Vertex> conditionMap = new TreeMap<>();
-
 		String urlStr = "https://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=results&down_flds=shown&down_fmt=plain&show_down=Y&term=";
 		urlStr += searchTerm;
 		URL url = new URL(urlStr);
@@ -123,7 +135,7 @@ public class App {
 					ClinicalStudy study = (ClinicalStudy) obj;
 					Files.delete(tf);
 
-					addStudyToGraph(study, g, sponsorMap, conditionMap);
+					addStudyToGraph(study, g);
 					trialCount++;
 				}
 			}
@@ -141,11 +153,8 @@ public class App {
 	 * 
 	 * @param study
 	 * @param g
-	 * @param sponsorMap
-	 * @param conditionMap
 	 */
-	private void addStudyToGraph(ClinicalStudy study, Graph g, Map<String, Vertex> sponsorMap,
-			Map<String, Vertex> conditionMap) {
+	private void addStudyToGraph(ClinicalStudy study, Graph g) {
 
 		String nctId = study.getIdInfo().getNctId();
 
@@ -153,95 +162,135 @@ public class App {
 		// Add nodes
 		//
 
-		String orgStudyId = study.getIdInfo().getOrgStudyId() != null ? study.getIdInfo().getOrgStudyId() : "";
-		String briefTitle = study.getBriefTitle() != null ? study.getBriefTitle() : "";
-		String overallStatus = study.getOverallStatus() != null ? study.getOverallStatus() : "";
-		String phase = study.getPhase() != null ? study.getPhase().toString() : "";
 		String studyType = study.getStudyType() != null ? study.getStudyType().toString() : "";
+		String briefTitle = getStringOrEmpty(study.getBriefTitle());
+		String overallStatus = getStringOrEmpty(study.getOverallStatus());
+		String phase = study.getPhase() != null ? study.getPhase().toString() : "";
 		Long enrollment = study.getEnrollment() != null ? study.getEnrollment().getValue().longValue() : 0;
-		Vertex ct = g.addVertex(T.label, "trial", "study_id", nctId, "org_study_id", orgStudyId, "title", briefTitle,
-				"overall_status", overallStatus, "phase", phase, "study_type", studyType, "enrollment", enrollment);
+		String orgStudyId = getStringOrEmpty(study.getIdInfo().getOrgStudyId());
+		EligibilityStruct studyEligibility = study.getEligibility();
+		String genderStr = studyEligibility.getGender().toString();
+		String minAge = studyEligibility.getMinimumAge();
+		String maxAge = studyEligibility.getMaximumAge();
+		Vertex clinicalTrial = g.addVertex(T.label, "trial", "study_id", nctId, "org_study_id", orgStudyId, "title",
+				briefTitle, "overall_status", overallStatus, "phase", phase, "study_type", studyType, "enrollment",
+				enrollment, "gender", genderStr, "minAge", minAge, "maxAge", maxAge);
+
+		if (study.getStartDate() != null) {
+			String startDateStr = study.getStartDate().getValue();
+			try {
+				int startYear = getYear(startDateStr);
+				if (startYear < 1900) {
+					LOG.warning(nctId + " has a likely invalid start year: " + startDateStr);
+				} else {
+					clinicalTrial.property("start_year", startYear);
+				}
+			} catch (ParseException e) {
+				LOG.warning(nctId + " does not have a valid start year: " + startDateStr);
+			}
+		}
 
 		SponsorsStruct ssList = study.getSponsors();
 		String sponsorAgency = ssList.getLeadSponsor().getAgency();
-		final Vertex sv1 = sponsorMap.get(sponsorAgency);
-		// String studyType = study.getStudyType() != null ?
-		// study.getStudyType().toString()
-		// : "none";
-		// String whyStopped = study.getWhyStopped() != null ?
-		// study.getWhyStopped() : "none";
-		// String startDate = study.getStartDate() != null ?
-		// study.getStartDate() : "none";
-		// String completionDate = study.getCompletionDate() != null ?
-		// study.getCompletionDate()
-		// .getValue() : "none";
-		final Vertex sv = sv1 != null ? sv1
-				: g.addVertex(T.label, "sponsor", "name", sponsorAgency, "class", sponsorAgency);
-		// , "studyType", studyType, "startDate",
-		// startDate, "endDate", completionDate, "phase",
-		// study.getPhase().toString(),
-		// "whystopped", whyStopped);
-		if (null == sv1) {
-			sponsorMap.put(sponsorAgency, sv);
-		}
-		addCount(sv);
+		AgencyClassEnum agencyClass = ssList.getLeadSponsor().getAgencyClass();
+		final Vertex sv = getOrCreateSponsorVertex(g, sponsorAgency, agencyClass.toString());
 
-		study.getSponsors().getCollaborator().forEach(collabAgency -> {
-			String collab = collabAgency.getAgency();
-			final Vertex cv1 = sponsorMap.get(collab);
-			final Vertex cv = cv1 != null ? cv1
-					: g.addVertex(T.label, "sponsor", "name", collab, "class",
-							collabAgency.getAgencyClass().toString());
-			if (null == cv1) {
-				sponsorMap.put(collab, cv);
-			}
-			addCount(cv);
-		});
-
-		study.getCondition().forEach(c -> {
-			String c2 = getNormalizedConditionName(c);
-			Vertex cVt = conditionMap.get(c2);
-			if (cVt == null) {
-				cVt = g.addVertex(T.label, "condition", "name", c2);
-			}
-			addCount(cVt);
-			conditionMap.put(c2, cVt);
-		});
-
-		//
-		// Add edges
-		//
+		List<String> studyConditions = study.getCondition();
 
 		// sponsor -> condition
 		// sponsor -> trial
-		study.getCondition().forEach(c -> {
-			String c2 = getNormalizedConditionName(c);
-			Vertex cVt = conditionMap.get(c2);
-			sv.addEdge("researches", cVt, "nct", nctId);
-			sv.addEdge("sponsors", ct, "nct", nctId);
+		studyConditions.forEach(c -> {
+			Vertex conditionVertex = getOrCreateConditionVertex(g, c);
+			sv.addEdge("researches", conditionVertex, "nct", nctId);
+			sv.addEdge("sponsors", clinicalTrial, "nct", nctId);
 		});
 
 		study.getSponsors().getCollaborator().forEach(collabAgency -> {
 			// sponsor -> collaborators
 			String collab = collabAgency.getAgency();
-			final Vertex cv = sponsorMap.get(collab);
 
-			sv.addEdge("leads", cv, "nct", nctId);
-			cv.addEdge("collaborates", sv, "nct", nctId);
+			AgencyClassEnum collabClass = collabAgency.getAgencyClass();
+			final Vertex collabVertex = getOrCreateSponsorVertex(g, collab, collabClass.toString());
+
+			sv.addEdge("leads", collabVertex, "nct", nctId);
+			collabVertex.addEdge("collaborates", sv, "nct", nctId);
 
 			// collaborators -> condition
 			// collaborators -> trial
-			// trial -> conditons
-			study.getCondition().forEach(c -> {
-				String c2 = getNormalizedConditionName(c);
-				Vertex cVt = conditionMap.get(c2);
-				cv.addEdge("researches", cVt, "nct", nctId);
-				cv.addEdge("cosponsors", ct, "nct", nctId);
-				ct.addEdge("covers", cVt);
+			// trial -> conditions
+			studyConditions.forEach(c -> {
+				Vertex conditionVertex = getOrCreateConditionVertex(g, c);
+				collabVertex.addEdge("researches", conditionVertex, "nct", nctId);
+				collabVertex.addEdge("cosponsors", clinicalTrial, "nct", nctId);
+				clinicalTrial.addEdge("covers", conditionVertex);
+			});
+
+			// trial -> locations
+			study.getLocation().forEach(l -> {
+				Vertex locationVertex = getOrCreateLocationVertex(g, l.getFacility());
+				String facilityName = getStringOrEmpty(l.getFacility().getName());
+				clinicalTrial.addEdge("location", locationVertex, "location_name", facilityName);
 			});
 
 		});
 
+	}
+
+	private int getYear(String startDateStr) throws ParseException {
+		Date startDate = NCT_DATE_FORMAT_1.parse(startDateStr);
+		Calendar c = Calendar.getInstance();
+		c.setTime(startDate);
+		int startYear = c.get(Calendar.YEAR);
+		if (startYear <= 31) {
+			startDate = NCT_DATE_FORMAT_2.parse(startDateStr);
+			c.setTime(startDate);
+			startYear = c.get(Calendar.YEAR);
+		}
+		return startYear;
+	}
+
+	/**
+	 * 
+	 * @param g
+	 * @param facility
+	 * @return
+	 */
+	private Vertex getOrCreateLocationVertex(Graph g, FacilityStruct facility) {
+		AddressStruct locationAddress = facility.getAddress();
+
+		String city = getStringOrEmpty(locationAddress.getCity());
+		String state = getStringOrEmpty(locationAddress.getState());
+		String zip = getStringOrEmpty(locationAddress.getZip());
+		String country = getStringOrEmpty(locationAddress.getCountry());
+		String locationString = city + " " + state + " " + zip + " " + country;
+		GraphTraversal<Vertex, Vertex> locationIter = g.traversal().V().has(VERTEX_PROPERTY_LOCATION_FULL_ADDRESS,
+				locationString);
+		final Vertex locationVertex = locationIter.hasNext() ? locationIter.next()
+				: g.addVertex(T.label, "location", VERTEX_PROPERTY_LOCATION_FULL_ADDRESS, locationString, "city", city,
+						"state", state, "zip", zip, "country", country);
+		return locationVertex;
+	}
+
+	private Vertex getOrCreateConditionVertex(Graph g, String conditionName) {
+		String c2 = getNormalizedConditionName(conditionName);
+		GraphTraversal<Vertex, Vertex> conditionIter = g.traversal().V().has(VERTEX_PROPERTY_CONDITION_NAME, c2);
+		final Vertex cVt = conditionIter.hasNext() ? conditionIter.next()
+				: g.addVertex(T.label, "condition", VERTEX_PROPERTY_CONDITION_NAME, c2);
+		return cVt;
+	}
+
+	/**
+	 * 
+	 * @param g
+	 * @param sponsorAgency
+	 * @param agencyClass
+	 * @return
+	 */
+	private Vertex getOrCreateSponsorVertex(Graph g, String sponsorAgency, String agencyClass) {
+		GraphTraversal<Vertex, Vertex> sponsorIter = g.traversal().V().has(VERTEX_PROPERTY_SPONSOR_NAME, sponsorAgency);
+		final Vertex sv = sponsorIter.hasNext() ? sponsorIter.next()
+				: g.addVertex(T.label, "sponsor", VERTEX_PROPERTY_SPONSOR_NAME, sponsorAgency, "class", agencyClass);
+		return sv;
 	}
 
 	private String getNormalizedConditionName(String c) {
@@ -249,8 +298,7 @@ public class App {
 		return c2;
 	}
 
-	private void addCount(Vertex cVt) {
-		Integer count = (Integer) cVt.property("count").orElse(new Integer(0));
-		cVt.property("count", count + 1);
+	private String getStringOrEmpty(String parm) {
+		return parm != null ? parm : "";
 	}
 }
